31d52378c1a66458b57c1937ff6cd59a3c31e844
Citrus <bbatchelder@proton.me>
2025-01-01 18:46:40 -0700

    Add styles and changes to function

--- java-script.js
+++ java-script.js
@@ -1,8 +1,16 @@
-let parentDiv = document.querySelector('.container');
+console.log('I am working');
 
-function createGrid (numberOfSquares = 16) {
-    for (let i = 0; i < numberOfSquares; i++) {
+const container = document.querySelector('.container');
+
+function createGrid (gridAmount) {
+    let gridCounter = 0;
+    for (let i = 0; i < gridAmount * gridAmount; i++) {
+            console.log(gridCounter);
             let newBox = document.createElement('div');
             newBox.classList.add('grid');
+            container.appendChild(newBox);
+            gridCounter++
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => createGrid(16));
