4018117a382951a3d1ae1e9db3d73c95caf62a84
Citrus <bbatchelder@proton.me>
2025-01-01 19:13:53 -0700

    Slight adjustments to styles and debug info to function

--- java-script.js
+++ java-script.js
@@ -7,6 +7,7 @@ function createGrid (gridAmount) {
     for (let i = 0; i < gridAmount * gridAmount; i++) {
             console.log(gridCounter);
             let newBox = document.createElement('div');
+            newBox.textContent = '.';
             newBox.classList.add('grid');
             container.appendChild(newBox);
             gridCounter++
