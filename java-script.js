018aa2c3e0bc0aef5def7f9518780a5322bccb46
Citrus <bbatchelder@proton.me>
2025-01-07 20:50:02 -0700

    Grid is being made correctly, need to make it look pretty

--- java-script.js
+++ java-script.js
@@ -5,11 +5,22 @@ const container = document.querySelector('.container');
 function createGrid (amountPerSide) {
     let gridCounter = 0;
     for (let i = 0; i < amountPerSide * amountPerSide; i++) {
-            console.log(gridCounter);
-            let newBox = document.createElement('div');
-            newBox.classList.add('grid');
-            container.appendChild(newBox);
-            gridCounter++
+            // console.log(gridCounter);
+            let cubeCounter = i + 1;
+            if(cubeCounter % amountPerSide == 0) {
+                let newBox = document.createElement('div');
+                newBox.classList.add('grid');
+                container.appendChild(newBox);
+                let endingDiv = document.createElement('div');
+                endingDiv.classList.add('endingDiv');
+                container.appendChild(endingDiv);
+                console.log(i);
+            } else {
+                let newBox = document.createElement('div');
+                newBox.classList.add('grid');
+                container.appendChild(newBox);
+            }
+            // gridCounter++
     }
 }
 
